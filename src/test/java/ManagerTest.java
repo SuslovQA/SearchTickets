c23a42b31e03c35051f572050151028604d9a5f1
Evgeny <evgenijsuslov08683@gmail.com>
2025-11-29 21:39:09 +0500

    add flightTimeComparator for Manager. Update ManagerTest

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -2,13 +2,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
+
 public class ManagerTest {
     Repository repo = new Repository();
     Manager manager = new Manager(repo);
+    TicketByFlightTimeDescComparator ticketByFlightTimeDesc = new TicketByFlightTimeDescComparator();
 
     Ticket ticket1 = new Ticket(1, 10_000, "SVX", "ROV", 150);
     Ticket ticket2 = new Ticket(2, 15_000, "SVX", "ROV", 170);
-    Ticket ticket3 = new Ticket(3, 25_000, "SVX", "IKT", 240);
+    Ticket ticket3 = new Ticket(3, 25_000, "SVX", "ROV", 140);
 
     @BeforeEach
     public void setup() {
@@ -17,26 +20,27 @@ public class ManagerTest {
         manager.save(ticket3);
     }
 
-    @Test
-    public void shouldTwoTickets() {
-        Ticket[] expected = {ticket1, ticket2};
-        Ticket[] actual = manager.findAll("SVX", "ROV");
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldFindOneTicket() {
-        Ticket[] expected = {ticket3};
-        Ticket[] actual = manager.findAll("SVX", "IKT");
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
 
     @Test
     public void shouldNotFindTickets() {
         Ticket[] expected = {};
-        Ticket[] actual = manager.findAll("SVX", "PHE");
+        Ticket[] actual = manager.findAll("SVX", "PHE", ticketByFlightTimeDesc);
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindWithFlightTimeSort() {
+        Ticket[] expected = {ticket3, ticket1, ticket2};
+        Ticket[] actual = manager.findAll("SVX", "ROV", ticketByFlightTimeDesc);
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindTickets2() {
+        Ticket[] expected = {};
+        Ticket[] actual = manager.findAll("ROV", "ROV", ticketByFlightTimeDesc);
 
         Assertions.assertArrayEquals(expected, actual);
     }
